7a491d89f90cf1e68615ecaa97b25efc867b8fa8
Phumthawan <knzphumthawan@gmail.com>
2019-05-30 04:50:20 +0700

    add next page

--- pages/index.js
+++ pages/index.js
@@ -2,24 +2,27 @@ import fetch from 'isomorphic-fetch';
 import Error from 'next/error';
 import StoryList from '../components/StoryList';
 import Layout from '../components/Layout';
+import Link from 'next/link';
 
 class Index extends React.Component {
-    static async getInitialProps() {
+
+    static async getInitialProps({ req, res, query }) {
         let stories;
+        let page;
         try {
-            const response = await fetch('http://node-hnapi.herokuapp.com/news?page=1');
+            page = Number(query.page) || 1;
+            const response = await fetch(`http://node-hnapi.herokuapp.com/news?page=${page}`);
             stories = await response.json();
         } catch (error) {
             console.log(error)
             stories = [];
         }
 
-
-        return { stories };
+        return { page, stories };
     }
 
     render() {
-        const { stories } = this.props;
+        const { stories, page } = this.props;
         if (stories == 0) {
             return <Error statusCode={503} />
         }
@@ -27,6 +30,22 @@ class Index extends React.Component {
         return (
             <Layout title="Hacker News" description="A hacker News Clone made with NextJS">
                 <StoryList stories={stories} />
+                <footer>
+                    <Link href={`/?page=${page + 1}`}>
+                        <a>Nextpage ({page+1})</a>
+                    </Link>
+                </footer>
+
+                <style jsx>{`
+                    footer{
+                        padding: 1em;
+                    }
+                    footer a {
+                        font-weight:bold;
+                        color:black;
+                        text-decoration:none;
+                    }
+                `}</style>
             </Layout>
         )
 
