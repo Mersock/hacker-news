f69ab1b6ea1b8ec28325c269d57505cb8931d295
Phumthawan <knzphumthawan@gmail.com>
2019-06-04 03:47:16 +0700

    add comment page

--- pages/story.js
+++ pages/story.js
@@ -1,6 +1,7 @@
 import Layout from '../components/Layout';
 import fetch from 'isomorphic-fetch';
 import Error from 'next/error';
+import CommentList from '../components/CommentList';
 
 
 class Story extends React.Component {
@@ -35,6 +36,13 @@ class Story extends React.Component {
                         <strong>{story.comments_count} comments</strong>
                         <strong>{story.time_ago}</strong>
                     </div>
+
+                    {story.comments.length > 0 ? (
+                        <CommentList comments={story.comments} />
+                    ) : (
+                            <div>No comments for this story</div>
+                        )
+                    }
                 </main>
 
                 <style jsx>{`
